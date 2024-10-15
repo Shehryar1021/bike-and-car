3ade68175e374c7c95afde10e830241a7be60826
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-15 16:35:00 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("SELECT YOUR RIDE :")
+print("1. BIKE")
+print("2. CAR")
+choice=int(input("ENTER YOUR CHOICE"))
+"if choice == 1:"
+print("WHAT TYPE OF BIKE? ")
+print("1. SCOOTY\n")
+print("2. SCOOTER\n")
+choice2=int(input("ENTER YOUR CHOICE 2"))
+"if choice2 == 1:"
+print("YOU HAVE SELECTED SCOOTY")
+else:
+print("YOU HAVE SELECTED SCOOTER")
+elif("choice == 2"):
+print("WHAT TYPE OF CAR? ")
+print("1.SEDAN")
+print("2.XUV")
+choice3=int(input("ENTER YOUR CHOICE 3"))
+print("YOU HAVE SELECTED SEDAN")
+else:
+print("YOU HAVE SELECTED XUV")
+else:
+print("WRONG CHOICE")
